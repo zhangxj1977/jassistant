07cc0245353a4a031892aac8e754a5c0f8979340
sji_zhang_x <sji_zhang_x@K111CL164.K111.shareddev.local>
2015-04-24 17:58:36 +0900

    jar/shellファイル更新

--- jas/run.cmd
+++ jas/run.cmd
@@ -1,7 +1,19 @@
 @echo off
-setlocal enabledelayedexpansion
+@setlocal enabledelayedexpansion
+
+ECHO -------------------------------
+ECHO Running jasssitant client
+ECHO -------------------------------
+
 set basepath=%~dp0
 set jarfiles=%basepath%lib\*.jar
 set classpath=.\jassistant.jar
 for %%i in (%jarfiles%) do (set classpath=!classpath!;%%i)
-java org.jas.gui.Main config
+
+if exist "C:\Program Files\Java\jre7" (
+    set JAVA_HOME=C:\Program Files\Java\jre7
+) else if exist "C:\Program Files\Java\jre6" (
+    set JAVA_HOME=C:\Program Files\Java\jre6
+)
+
+@start "jassistant" "%JAVA_HOME%\bin\javaw.exe" -Dsun.java2d.ddoffscreen=false -cp %classpath% org.jas.gui.Main config
